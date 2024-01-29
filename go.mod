448c0be35e60861eae112d8058bbe46b3c72c50b
kee <kee@email.com>
2024-01-29 14:38:08 +0530

    adding mysql_conn.go file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module nsdq_sql_conn
+
+go 1.21.3
+
+require github.com/go-sql-driver/mysql v1.7.1
---
+++ mysql_conn.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:keerthi1234@tcp(localhost:3306)/testdb")
+
+	if err != nil {
+		fmt.Println("error validating sql.Open arguments")
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("error verifying connection with db.Ping")
+		panic(err.Error())
+	}
+
+	insert, err := db.Query("INSERT INTO `testdb`.`students` (`id`, `firstname`, `lastname`) VALUES ('3', 'keerthi', 'shekar');")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
+	fmt.Println("Successful Connection to Database!")
+}
